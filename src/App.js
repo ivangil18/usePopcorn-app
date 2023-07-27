137b44450049ef5847676b9d428f8c2e6b0e3f76
Ivan Gil <igil.dev@gmail.com>
2023-07-27 01:51:51 -0400

    movies prop drilling problem fixed

--- src/App.js
+++ src/App.js
@@ -54,18 +54,25 @@ export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} />
+      <NavBar>
+        <Search />
+        <NumResults movies={movies} />
+      </NavBar>
+      <Main>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
 
-function NavBar({ movies }) {
+function NavBar({ children }) {
   return (
-    <nav className="nav-bar" movies={movies}>
+    <nav className="nav-bar">
       <Logo />
-      <Search />
-      <NumResults movies={movies} />
+      {children}
     </nav>
   );
 }
@@ -100,16 +107,11 @@ function NumResults({ movies }) {
   );
 }
 
-function Main({ movies }) {
-  return (
-    <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
-    </main>
-  );
+function Main({ children }) {
+  return <main className="main">{children}</main>;
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
   function handleToggle() {
     setIsOpen1((open) => !open);
@@ -117,7 +119,7 @@ function ListBox({ movies }) {
   return (
     <div className="box">
       <Button onClick={handleToggle}>{isOpen1 ? "–" : "+"}</Button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && children}
     </div>
   );
 }
